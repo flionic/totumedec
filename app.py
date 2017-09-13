f82453ef9676fa682cf99c32ace7cfc0c77da411
Bionic Leha <bionic.mods@gmail.com>
2017-09-13 04:30:50 +0300

    Add comments
    Took 1 hour 30 minutes

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ print('Copyrights © ⏤ E-declaration Bot for Totum by Bionic Inc 2017')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Main messages
 msg_start = '*Оберіть необхідний розділ:*'
 msg_contacts = '''
 *Зворотній зв’язок*
@@ -28,6 +29,7 @@ http://totum.com.ua/
 
 
 def cmd_start(bot, update):
+    # Inline MainMenu
     main_menu = InlineKeyboardMarkup([
         [InlineKeyboardButton("Загальна інформація", callback_data='m1_1')],
         [InlineKeyboardButton("Об’єкти декларування", callback_data='m1_2')],
@@ -39,6 +41,7 @@ def cmd_start(bot, update):
 
 
 def cmd_start_t(bot, update):
+    # KeyboardMarkup MainMenu
     reply_keyboard = [
         ['Загальна інформація'],
         ['Об’єкти декларування'],
@@ -49,21 +52,36 @@ def cmd_start_t(bot, update):
                      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
 
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
 def main():
+    # Create EventHandler and trying to pass it bot's token.
     try:
         updater = Updater(token=os.environ.get('token'))
         dp = updater.dispatcher
     except ValueError:
-        logger.error("Token was not found.")
+        logger.error("Token was not found")
     except telegram.error.InvalidToken:
-        logger.exception("Updater: Invalid token")
+        logger.exception("Token is invalid")
     else:
+        # Add bot handlers
         dp.add_handler(CommandHandler('start', cmd_start))
         dp.add_handler(CommandHandler('start_test', cmd_start_t))
         dp.add_handler(MessageHandler(Filters.text, cmd_start))
 
-        logger.info("Start polling")
+        # log all errors
+        dp.add_error_handler(error)
+
+        # Start Bot
+        logger.info("Start bot")
         updater.start_polling()
+
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        updater.idle()
 
 
 if __name__ == '__main__':
